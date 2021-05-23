26bb921684b1c5a55f3ae9305d0d73a73be0a42d
Jeral <jeral.cris@gmail.com>
2021-05-23 00:39:02 -0600

    Validar la extension del archivo

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -2,13 +2,31 @@ const path = require('path');
 const { response } = require('express');
 
 const cargarArchivo = (req, res = response) => {
+   // Validando que venga el archivo desde req.files
    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
       res.status(400).json('No hay archivos que subir');
       return;
    }
 
+   // extraemos el archivo
    const { archivo } = req.files;
 
+   //    Separamos el nombre del archivo
+   const nombreCortado = archivo.name.split('.');
+
+   // Extraer la extension del archivo
+   const extension = nombreCortado[nombreCortado.length - 1];
+
+   // Validar la extension del archivo
+   const extansionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
+
+   if (!extansionesValidas.includes(extension)) {
+      return res.status(400).json({
+         msg: `La extension ${extension} no es permitida, ${extansionesValidas}`,
+      });
+   }
+
+   //    Cargar archivos
    const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
 
    archivo.mv(uploadPath, (err) => {
