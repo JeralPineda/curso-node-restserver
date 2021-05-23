35d437173a0e6a4bac0e90ceb4766a038dd18ae5
Jeral <jeral.cris@gmail.com>
2021-05-23 17:47:41 -0600

    modificacion en el msg del midleware

--- middlewares/validar-archivo.js
+++ middlewares/validar-archivo.js
@@ -3,7 +3,7 @@ const { response } = require('express');
 const validarArchivoSubir = (req, res = response, next) => {
    // Validando que venga el archivo desde req.files
    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-      return res.status(400).json({ msg: 'No hay archivos que subir - validar archivo' });
+      return res.status(400).json({ msg: 'No hay archivos que subir' });
    }
 
    next();
